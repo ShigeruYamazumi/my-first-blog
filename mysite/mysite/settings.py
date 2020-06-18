354f1bd22d91a4be7c5f1292dc47687b8f19a8e3
Shigeru Yamazumi <sysma@smile.email.ne.jp>
2020-06-18 09:54:58 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,3 +120,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+NTERNAL_IPS = [
+        '127.0.0.1',
+]
+
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+    INSTALLED_APPS += (
+            'debug_toolbar',
+    )
+    MIDDLEWARE += (
+            'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    DEBUG_TOOLBAR_CONFIG = {
+            'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
+
