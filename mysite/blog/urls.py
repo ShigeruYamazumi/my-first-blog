250eb2f080d787c93a2e13a9991c9825e8a3f0ca
Shigeru Yamazumi <sysma@smile.email.ne.jp>
2020-06-19 11:13:39 +0900

    Add Kinō

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'), # リスト表示
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'), # 新規
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), # 編集
+    path('drafts/', views.post_draft_list, name='post_draft_list'), # 草稿ページ
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'), # 公開
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'), # 削除
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
